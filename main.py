c1cb043dc1edad61479a85bea8589d20a78bd524
JFVadiaz25 <vadiazjf@gmail.com>
2026-03-15 11:21:35 -0500

    Semaforo con Activación por microfono

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ mic.atten(ADC.ATTN_11DB) # rango 0–3.6V
 mic.width(ADC.WIDTH_12BIT) # resolución 12 bits (0-4095)
 
 GPIO=const(0x3FF44004)
+semaforo=False
 
 while True:
     value = mic.read()
@@ -20,27 +21,23 @@ while True:
     sleep(0.1)
 
     if value > 500: #Situacion 0
+        semaforo=True
+    if semaforo:
         mem32[GPIO]=0B0110000000000100000000000000 #situacion 1
         sleep(5)
         for i in range(3): #Parpadea situacion 1 verde
-            mem32[GPIO]=0B0100000000000100000000000000
-            sleep(1)
             mem32[GPIO]=0B0110000000000100000000000000
             sleep(1)
-        for i in range(3): #Situacion 2 parpadea verde y amarillo prende
-            mem32[GPIO]=0B0100000000000100000000000010
+            mem32[GPIO]=0B0100000000000100000000000000
             sleep(1)
-            mem32[GPIO]=0B0110000000000100000000000010
-            sleep(1)
-        mem32[GPIO]=0B1000000000010000000000001000
+        mem32[GPIO]=0B0100000000100110000000000000#Situacion 2 amarillo prende
         sleep(5)
-        for i in range(3): #Situacion 3 
+        mem32[GPIO]=0B1000000000010001000000000000 #Situacion 3
+        sleep(5)
+        for i in range(3): #Situacion 3 parpadea verde
+            mem32[GPIO]=0B1000000000010001000000000000
+            sleep(1)
             mem32[GPIO]=0B0000000000010000000000000000
             sleep(1)
-            mem32[GPIO]=0B1000000000010000000000001000
-            sleep(1)
-        for i in range(3): #Situacion 4 
-            mem32[GPIO]=0B0000000000110000000000000000
-            sleep(1)
-            mem32[GPIO]=0B1000000000110000000000001000
-            sleep(1)
+        mem32[GPIO]=0B0000000000110110000000000000 #Situacion 4 amarillo prende
+        sleep(5)
