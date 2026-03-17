109255d14225435fffd08e4c049babc69026accc
JFVadiaz25 <vadiazjf@gmail.com>
2026-03-16 22:56:02 -0500

    Semaforo que depende de contador

--- main.py
+++ main.py
@@ -2,13 +2,8 @@
 from machine import Pin, ADC, mem32, const, Timer
 from time import sleep
 
-def Peaton(pin):
-    global BotonPeaton
-    print("Activo el semaforo peatonal")
-    BotonPeaton=1
-
-vectorpines = [12,13,26,25,17,16,27,14]
-
+#Pines Semaforo
+vectorpines = [12,13,3,25,17,16,27,14]
 for i in vectorpines:
     Pin(i, Pin.OUT)
 
@@ -16,14 +11,17 @@ mic = ADC(Pin(35))       # ADC en GPIO35
 mic.atten(ADC.ATTN_11DB) # rango 0–3.6V
 mic.width(ADC.WIDTH_12BIT) # resolución 12 bits (0-4095)
 
-GPIO=const(0x3FF44004)
-semaforo=False
 
-pulsador=Pin(18,Pin.IN,Pin.PULL_DOWN)
+#Boton Peatonal
+def Peaton(pin):
+    global BotonPeaton
+    print("Activo el semaforo peatonal")
+    BotonPeaton=1
+
+pulsador=Pin(39,Pin.IN,Pin.PULL_DOWN)
 pulsador.irq(trigger=Pin.IRQ_RISING,handler=Peaton)
 
-BotonPeaton=0
-
+#Pines Display Y Mux
 pinesdisplay=[5,23,22,21,0,2,4]
 
 for i in pinesdisplay:
@@ -43,86 +41,124 @@ display= [[1,1,1,1,1,1,0],
           [1,1,1,1,1,1,1],
           [1,1,1,0,0,1,1]]
 
-contador = 10
-muxContador = 0
-contadorActivo = False
-
 def conteo(Timer):
-
     global contador
     global muxContador
+    global contadorTiempo
     muxContador += 1
+    contadorTiempo += 1
     if muxContador == 100:
-
         if contadorActivo and contador > 0:
             contador -= 1
             print(contador)
-
         muxContador = 0
+
 temporizador = Timer(0)
 temporizador.init(period=10, mode=Timer.PERIODIC, callback=conteo)
 
 def actualizar_display():
-
     if contadorActivo and contador > 0:
-
         mux[0].value(1)
         mux[1].value(1)
 
         if muxContador % 2 == 0:
-
             for i in range(7):
                 Pin(pinesdisplay[i], value=display[contador % 10][i])
-
             mux[1].value(0)
 
         else:
-
             decena = contador // 10
-
             for i in range(7):
                 Pin(pinesdisplay[i], value=display[decena][i])
-
             mux[0].value(0)
+
+
+#Valores iniciales
+contador = 10
+muxContador = 0
+contadorActivo = False
+BotonPeaton=0
+GPIO=const(0x3FF44004)
+semaforo=False
+
+estado=1
+contadorTiempo=0 
+parpadeo=0
+value = 0
 
 while True:
     actualizar_display()
-    value = mic.read()
-    print(value)
-    sleep(0.1)
+    if muxContador % 20 == 0:
+        value = mic.read()
+        print(value)
 
     if value > 500: #Situacion 0
         semaforo=True
     if semaforo:
-        mem32[GPIO]=0B0110000000000100000000000000 #situacion 1
-        sleep(5)
-        for i in range(3): #Parpadea situacion 1 verde
-            mem32[GPIO]=0B0110000000000100000000000000
-            sleep(1)
-            mem32[GPIO]=0B0100000000000100000000000000
-            sleep(1)
-        mem32[GPIO]=0B0100000000100110000000000000#Situacion 2 amarillo prende
-        sleep(5)
-        mem32[GPIO]=0B1000000000010001000000000000 #Situacion 3
-        contador = 10
-        contadorActivo = True
+        if estado == 1:
+            mem32[GPIO]=0B0010000000000100000000001000 #situacion 1
+            if contadorTiempo >= 500:   # 5 segundos
+                contadorTiempo = 0
+                estado = 2
+            
+        elif estado == 2: #Parpadea verde situacion 1
+            if contadorTiempo >= 50:
+                contadorTiempo=0
+                parpadeo+=1
+                if parpadeo %2:
+                    mem32[GPIO]=0B0010000000000100000000001000
+                else:
+                    mem32[GPIO]=0B0000000000000100000000001000
+                if parpadeo >=6:
+                    estado=3
+                    parpadeo=0
 
-        while contador > 0:
-            actualizar_display()
-            sleep(0.01)
-        contadorActivo=False
-        mem32[GPIO]=0B0000000000110110000000000000 #Situacion 4 amarillo prende
-        sleep(5)
-        if BotonPeaton:
-            print("inicia el ciclo peatonal")
+        elif estado == 3:
+            mem32[GPIO]=0B0000000000100110000000001000 #Situacion 2 amarillo prende
+            if contadorTiempo >= 500:
+                contadorTiempo=0
+                estado=4
+
+        elif estado == 4:
+            mem32[GPIO]=0B1000000000010001000000000000
+            if contadorTiempo == 1:
+                contador = 10
+                contadorActivo = True
+
+            if contadorTiempo >= 1000:
+                contadorTiempo = 0
+                contadorActivo = False
+                estado = 5
+        elif estado == 5:
+            mem32[GPIO]=0B0000000000110110000000000000 #Situacion 4 amarillo prende
+            if contadorTiempo >= 500:
+                contadorTiempo=0
+                if BotonPeaton:
+                    print("inicia el ciclo peatonal")   
+                    estado=6
+                else:
+                    estado=1 
+                    print("Termina el ciclo semaforo")
+        elif estado == 6:
             mem32[GPIO]=0B0000000000100110000000000000 #Situacion 5 peatonal verde
-            sleep(3)
+            if contadorTiempo >= 300:
+                contadorTiempo=0
+                estado=7
+        elif estado ==7:
             mem32[GPIO]=0B1100000000010000000000000000
-            sleep(5)
-            for i in range(3): #Situacion 5 parpadea verde
-                mem32[GPIO]=0B1100000000010000000000000000
-                sleep(1)
-                mem32[GPIO]=0B0100000000010000000000000000
-                sleep(1)
-            BotonPeaton=0
-        print("Termina el ciclo semaforo")
+            if contadorTiempo >=500:
+                    contadorTiempo=0
+                    estado=8
+        elif estado==8: #Situacion 5 parpadea verde
+            if contadorTiempo >= 50:
+                contadorTiempo=0
+                parpadeo+=1
+                if parpadeo %2:
+                    mem32[GPIO]=0B1100000000010000000000000000
+                else:
+                    mem32[GPIO]=0B0100000000010000000000000000
+                if parpadeo >=6:
+                    parpadeo=0
+                    BotonPeaton=0
+                    estado=1
+                    print("Termina el ciclo semaforo")
